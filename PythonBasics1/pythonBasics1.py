895803c27733705f2cad70bc5d4cf7875372f2bb
Mak <azadkha@gmail.com>
2022-09-21 09:15:25 -0400

    Activity 4.1 initial commit

--- PythonBasics1/pythonBasics1.py
+++ PythonBasics1/pythonBasics1.py
@@ -38,6 +38,11 @@ def has_lower_case(s):
 # if num is does not meet any of the above criteria or is less than
 # or equal to 0 return the num as a string
 def fizz_buzz(num):
-  # YOUR CODE HERE
-  
-  return
+  if num>0:
+    if num%3==0 and num%5==0:
+      return "FizzBuzz" 
+    elif num%5==0:
+      return "Buzz"
+    elif num%3==0:
+      return "Fizz"
+  return str(num)
