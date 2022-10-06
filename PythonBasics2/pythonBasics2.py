62f0f82f9a5bdce1bbe55d9335d4c4ac6929b1c2
Mak <azadkha@gmail.com>
2022-10-05 23:10:40 -0400

    PythonBasics2 – Update methods

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -10,34 +10,45 @@
 # returns the number of multiples of 3 in the range from 0
 # to n (including n).
 
-def count_threes(n):
-  count = 0
-  if n>2:
-    for i in range (0,n):
-      if i%3==0:
-        count+=1
-
-  return count
-
+def count_threes(s):
+    dict3 = {}
+    maxvalue = 0
+    maxkey = "0"
+    for i in s:
+        if int(i) % 3 == 0 and int(i) > 2:
+            # Adding key and values to dict3
+            if i in dict3:
+                dict3[i] += 1
+            else:
+                dict3[i] = 1
+    for key, value in dict3.items():
+        if value > maxvalue:
+            maxvalue = value
+            maxkey = key
+    return int(maxkey)
 
 # Part B. longest_consecutive_repeating_char
 # Define a function longest_consecutive_repeating_char(s) that takes
 # a string s and returns the character that has the longest consecutive repeat.
 def longest_consecutive_repeating_char(s):
-  alph = ''
-  countold = 0
-  countnew = 1
-  for i in range (0,len(s)-1):
-    countnew = 0
-    for j in range (i+1,len(s)):
-      if s[i]!=s[j]:
-        break
-      countnew += 1
-      if countnew >= countold:
-        countold = countnew
-        alph = s[i]
-  return (alph)
-# longest_consecutive_repeating_char('aaaaaafffffftttt')
+    dictList = {}
+    listKey = []
+    i, j = 0, 1
+    while i < len(s) and j < len(s):
+        dictList[s[i]] = 1
+        while j < len(s):
+            if s[i] != s[j]:
+                i = j
+                j = i + 1
+                break
+            dictList[s[i]] += 1
+            j += 1
+    x = max(dictList.values())
+    for key, value in dictList.items():
+        if value == x:
+            listKey.append(key)
+    return listKey
+
 
 # Part C. is_palindrome
 # Define a function is_palindrome(s) that takes a string s
@@ -46,8 +57,8 @@ def longest_consecutive_repeating_char(s):
 # forwards. Treat capital letters the same as lowercase ones
 # and ignore spaces (i.e. case insensitive).
 def is_palindrome(s):
-  s = s.replace(" ","")
-  srev = ''
-  for i in range(len(s)-1,-1,-1):
-    srev = srev+s[i]
-  return (srev.lower()==s.lower())
+    s = s.replace(" ", "")
+    srev = ''
+    for i in range(len(s) - 1, -1, -1):
+        srev = srev + s[i]
+    return srev.lower() == s.lower()
