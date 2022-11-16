72d5494b999b46040281ca61ba59d798c84e1c50
Mak <azadkha@gmail.com>
2022-11-15 22:45:49 -0500

    added registration functionality

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -5,14 +5,19 @@ import os  # os is used to get environment variables IP & PORT
 from flask import Flask  # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
-from flask import redirect, url_for
+from flask import redirect, url_for, session
 from database import db
 from models import Note as Note
 from models import User as User
+from forms import RegisterForm
+import bcrypt
 
 app = Flask(__name__)  # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# configure the secret key that will be used to by the app to secure session data
+app.config['SECRET_KEY'] = 'SE3155'
 #  Bind SQLAlchemy db object to this Flask app
 db.init_app(app)
 # Setup models
@@ -34,10 +39,14 @@ def index():
 @app.route('/notes')
 def get_notes():
     # retrieve user from database
-    a_user = db.session.query(User).filter_by(email='mazad@uncc.edu').one()
-    # retrieve notes from the database
-    my_notes = db.session.query(Note).all()
-    return render_template('notes.html', notes=my_notes, user=a_user)
+    # Check if the user is saved in session
+    if session.get('user'):
+        # retrieve notes from the database
+        my_notes = db.session.query(Note).filter_by(user_id=session['user_id']).all()
+
+        return render_template('notes.html', notes=my_notes, user=session['user'])
+    else:
+        return render_template(url_for('login'))
 
 
 @app.route('/notes/<note_id>')
@@ -108,6 +117,32 @@ def delete_note(note_id):
     db.session.commit()
 
     return redirect(url_for('get_notes'))
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        # salt and hash password
+        h_password = bcrypt.hashpw(
+            request.form['password'].encode('utf-8'), bcrypt.gensalt())
+        # get entered user data
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        # create user model
+        new_user = User(first_name, last_name, request.form['email'], h_password)
+        # add user to database and commit
+        db.session.add(new_user)
+        db.session.commit()
+        # save the user's name to the session
+        session['user'] = first_name
+        session['user_id'] = new_user.id  # access id value from user model of this newly added user
+        # show user dashboard view
+        return redirect(url_for('get_notes'))
+
+    # something went wrong - display register view
+    return render_template('register.html', form=form)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
